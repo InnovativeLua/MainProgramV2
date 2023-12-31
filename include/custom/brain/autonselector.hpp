676af8b986453cdf8c2f66096a7b45a8e9777439
MP <innovativelua@gmail.com>
2023-12-31 16:25:56 -0600

    Added auton selecting support base

--- include/custom/brain/autonselector.hpp
+++ include/custom/brain/autonselector.hpp
@@ -1,1 +1,14 @@
 #pragma once
+#include <vector>
+#include "custom/brain/auton.hpp"
+
+class AutonSelector {
+public:
+  std::vector<Auton> Autons;
+  int currentAutonPage;
+  int autonCount;
+  AutonSelector();
+  void callSelectedAuton();
+  void printSelectedAuton();
+  void addAutons(std::vector<Auton> autons);
+};
